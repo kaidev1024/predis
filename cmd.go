7aa922af541e48f715b89a85bbf1964f42bb6e0a
Xiaokai Li <xiaokai.li-1@hotmail.com>
2025-10-05 08:40:41 -0700

    minor fix

--- cmd.go
+++ cmd.go
@@ -1,14 +1,23 @@
 package predis
 
 import (
-	"context"
+	"encoding/json"
+	"fmt"
 	"time"
 )
 
-func Set(ctx context.Context, key string, value interface{}, expiration time.Duration) error {
+func set(key string, value any, expiration time.Duration) error {
 	return client.Set(ctx, key, value, expiration).Err()
 }
 
-func Get(ctx context.Context, key string) (string, error) {
+func Get(key string) (string, error) {
 	return client.Get(ctx, key).Result()
+}
+
+func Set(key string, value any, expiration time.Duration) error {
+	data, err := json.Marshal(value)
+	if err != nil {
+		return fmt.Errorf("error marshalling the struct")
+	}
+	return set(key, data, expiration)
 }
--- setup.go
+++ setup.go
@@ -1,15 +1,20 @@
 package predis
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/redis/go-redis/v9"
 )
 
 var client *redis.Client
+var ctx context.Context
 
-func Setup(addr string) {
+func Setup(addr string, ctx0 context.Context) {
 	client = redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
+		Addr:     fmt.Sprintf("%s:6379", addr),
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
+	ctx = ctx0
 }
