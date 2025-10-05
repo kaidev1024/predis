08231361e6a19fcbaeacd19fcece73162d1aa8c4
Xiaokai Li <xiaokai.li-1@hotmail.com>
2025-10-04 23:09:53 -0700

    add CreateKey

--- util.go
+++ util.go
@@ -1,7 +1,23 @@
 package predis
 
-import "github.com/redis/go-redis/v9"
+import (
+	"fmt"
+
+	"github.com/redis/go-redis/v9"
+)
 
 func IsNil(err error) bool {
 	return err == redis.Nil
+}
+
+func CreateKey(tableName string, cols ...string) string {
+	var key string
+	for i, col := range cols {
+		if i == 0 {
+			key = fmt.Sprintf("%s:%s", tableName, col)
+		} else {
+			key = fmt.Sprintf("%s_%s", key, col)
+		}
+	}
+	return key
 }
