7e740bfdd37460eb76a4317b6ccc64b9abc9b0fc
Xiaokai Li <xiaokai.li-1@hotmail.com>
2025-10-11 18:21:04 -0700

    add functions for handle struct

--- cmd.go
+++ cmd.go
@@ -11,14 +11,28 @@ func set(ctx context.Context, key string, value any, expiration time.Duration) e
 	return client.Set(ctx, key, value, expiration).Err()
 }
 
-func Get(ctx context.Context, key string) (string, error) {
-	return client.Get(ctx, key).Result()
-}
-
-func Set(ctx context.Context, key string, value any, expiration time.Duration) error {
+func SetStruct(ctx context.Context, key string, value any) error {
 	data, err := json.Marshal(value)
 	if err != nil {
-		return fmt.Errorf("error marshalling the struct")
+		return fmt.Errorf("marshal error: %w", err)
+	}
+	return set(ctx, key, data, defaultExpiration)
+}
+
+func SetStructWithExpiration(ctx context.Context, key string, value any, expiration time.Duration) error {
+	data, err := json.Marshal(value)
+	if err != nil {
+		return fmt.Errorf("marshal error: %w", err)
 	}
 	return set(ctx, key, data, expiration)
+}
+
+
+// Get JSON and unmarshal into any struct
+func GetStruct(ctx context.Context, key string, dest any) error {
+	val, err := client.Get(ctx, key).Result()
+	if err != nil {
+		return fmt.Errorf("redis get error: %w", err)
+	}
+	return json.Unmarshal([]byte(val), dest)
 }
--- setup.go
+++ setup.go
@@ -1,15 +1,27 @@
 package predis
 
 import (
+	"time"
+
 	"github.com/redis/go-redis/v9"
 )
 
 var client *redis.Client
+var defaultExpiration time.Duration = 24 * time.Hour
 
-func Setup(addr string) {
+func setUpClient(addr string) {
 	client = redis.NewClient(&redis.Options{
 		Addr:     addr,
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
+}
+
+func SetUp(addr string) {
+	setUpClient(addr)
+}
+
+func SetUpWithExpiration(addr string, expiration time.Duration) {
+	defaultExpiration = expiration
+	setUpClient(addr)
 }
