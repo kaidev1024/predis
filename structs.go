412dae1cbe2ba3421f7e2de743e095bab1eb88e4
Xiaokai Li <xiaokai.li-1@hotmail.com>
2025-10-11 20:16:46 -0700

    refactor structs

---
+++ structs.go
@@ -0,0 +1,33 @@
+package predis
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"time"
+)
+
+func SetStruct(ctx context.Context, key string, value any) error {
+	data, err := json.Marshal(value)
+	if err != nil {
+		return fmt.Errorf("marshal error: %w", err)
+	}
+	return set(ctx, key, data, defaultExpiration)
+}
+
+func SetStructWithExpiration(ctx context.Context, key string, value any, expiration time.Duration) error {
+	data, err := json.Marshal(value)
+	if err != nil {
+		return fmt.Errorf("marshal error: %w", err)
+	}
+	return set(ctx, key, data, expiration)
+}
+
+// GetStruct retrieves JSON and unmarshals into a pointer type using generics
+func GetStruct[T any](ctx context.Context, key string, dest *T) error {
+	val, err := get(ctx, key)
+	if err != nil {
+		return fmt.Errorf("redis get error: %w", err)
+	}
+	return json.Unmarshal([]byte(val), dest)
+}
--- util.go
+++ util.go
@@ -8,3 +8,7 @@ import (
 func set(ctx context.Context, key string, value any, expiration time.Duration) error {
 	return client.Set(ctx, key, value, expiration).Err()
 }
+
+func get(ctx context.Context, key string) (string, error) {
+	return client.Get(ctx, key).Result()
+}
