459d94ebfc22e282bc3731167ee03a9bc1cee782
Xiaokai Li <xiaokai.li-1@hotmail.com>
2025-10-12 23:20:09 -0700

    minor fix

--- setup.go
+++ setup.go
@@ -1,6 +1,7 @@
 package predis
 
 import (
+	"log"
 	"time"
 
 	"github.com/redis/go-redis/v9"
@@ -10,20 +11,28 @@ var client *redis.Client
 var coldDefaultExpiration time.Duration = time.Hour
 var hotDefaultExpiration time.Duration = 24 * time.Hour
 
-func setUpClient(addr string) {
-	client = redis.NewClient(&redis.Options{
-		Addr:     addr,
-		Password: "", // no password set
-		DB:       0,  // use default DB
-	})
+// func setUpClient(addr string) {
+// 	client = redis.NewClient(&redis.Options{
+// 		Addr:     addr,
+// 		Password: "", // no password set
+// 		DB:       0,  // use default DB
+// 	})
+// }
+
+func setUpClient(url string) {
+	opt, err := redis.ParseURL(url)
+	if err != nil {
+		log.Fatalf("failed to parse redis url: %v", err)
+	}
+	client = redis.NewClient(opt)
 }
 
-func SetUp(addr string) {
-	setUpClient(addr)
+func SetUp(url string) {
+	setUpClient(url)
 }
 
-func SetUpWithExpiration(addr string, coldExpiration, hotExpiration time.Duration) {
+func SetUpWithExpiration(url string, coldExpiration, hotExpiration time.Duration) {
 	coldDefaultExpiration = coldExpiration
 	hotDefaultExpiration = hotExpiration
-	setUpClient(addr)
+	setUpClient(url)
 }
