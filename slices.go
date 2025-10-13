2b60c263f7b85087e95b5af629b14d1de0e56baa
Xiaokai Li <xiaokai.li-1@hotmail.com>
2025-10-12 20:02:55 -0700

    add functions for slice

--- slices.go
+++ slices.go
@@ -1,2 +1,36 @@
 package predis
 
+import (
+	"context"
+	"encoding/json"
+)
+
+func RPush[T any](ctx context.Context, key string, items []T) error {
+	for _, item := range items {
+		data, err := json.Marshal(item)
+		if err != nil {
+			return err
+		}
+		if err := client.RPush(ctx, key, data).Err(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func LRange[T any](ctx context.Context, key string, start, stop int64) ([]T, error) {
+	vals, err := client.LRange(ctx, key, start, stop).Result()
+	if err != nil {
+		return nil, err
+	}
+
+	items := make([]T, 0, len(vals))
+	for _, v := range vals {
+		var item T
+		if err := json.Unmarshal([]byte(v), &item); err != nil {
+			return nil, err
+		}
+		items = append(items, item)
+	}
+	return items, nil
+}
